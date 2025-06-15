67ea5f361a03a2b6576f55e6f4b61e3c92eee4a1
ds7cw <delyan.shivachev@hotmail.com>
2025-06-15 23:40:09 +0300

    Add ollama llm setup

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_openai.embeddings import OpenAIEmbeddings
+# from langchain_ollama.embeddings import OllamaEmbeddings # Local LLM
+# from langchain_community.llms import Ollama # Local LLM
 from langchain_openai import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
@@ -20,12 +22,12 @@ def setup_qa_system(file_path):
         chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_documents(docs)
 
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings() # embeddings = OllamaEmbeddings(model='llama2') # Local LLM
     vector_store = FAISS.from_documents(chunks, embeddings)
 
     retriever = vector_store.as_retriever
     # The lower the temperature, the more conservative the answer
-    llm = ChatOpenAI(temperature=0, model_name='gpt-4o')
+    llm = ChatOpenAI(temperature=0, model_name='gpt-4o') # llm = Ollama(model='llama2')
 
     qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever)
 
